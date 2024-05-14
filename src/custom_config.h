afbb97edac0fe8ba7ad81f0a667adf030b6f90af
raopx <shaban@opx.slmail.me>
2024-05-14 19:54:57 +0200

    swap quote and ;

--- src/custom_config.h
+++ src/custom_config.h
@@ -4,7 +4,7 @@
 #pragma once
 
 
-#define MIRYOKU_ALPHAS_QWERTY \
+#define MIRYOKU_LAYER_BASE \
 q,                 w,                 e,                 r,                 t,                 y,                 u,                 i,                 o,                 p,                 \
 U_MT(a, met),      U_MT(s, alt),      U_MT(d, ctl),      U_MT(f, sft),      g,                 h,                 U_MT(j, sft),      U_MT(k, ctl),      U_MT(l, alt),      U_MT(;, met), \
 U_LT(z, U_BUTTON), U_MT(x, ralt),     c,                 v,                 b,                 n,                 m,                 U_COMM,            U_MT(., ralt),     U_LT(/, U_BUTTON), \
