8bb2be07fe985729019eb0478c568c72aa18640b
raopx <shaban@opx.slmail.me>
2024-05-14 22:43:12 +0200

    swap ctl and met

--- src/custom_config.h
+++ src/custom_config.h
@@ -6,6 +6,6 @@
 
 #define MIRYOKU_LAYER_BASE \
 q,                 w,                 e,                 r,                 t,                 y,                 u,                 i,                 o,                 p,                 \
-U_MT(a, met),      U_MT(s, alt),      U_MT(d, ctl),      U_MT(f, sft),      g,                 h,                 U_MT(j, sft),      U_MT(k, ctl),      U_MT(l, alt),      U_MT(;, met), \
+U_MT(a, ctl),      U_MT(s, alt),      U_MT(d, met),      U_MT(f, sft),      g,                 h,                 U_MT(j, sft),      U_MT(k, met),      U_MT(l, alt),      U_MT(U_QUOT, ctl), \
 U_LT(z, U_BUTTON), U_MT(x, ralt),     c,                 v,                 b,                 n,                 m,                 U_COMM,            U_MT(., ralt),     U_LT(/, U_BUTTON), \
 U_NP,              U_NP,              U_LT(esc, U_MEDIA),U_LT(spc, U_NAV),  U_LT(tab, U_MOUSE),U_LT(ent, U_SYM),  U_LT(bspc, U_NUM), U_LT(del, U_FUN),  U_NP,              U_NP
